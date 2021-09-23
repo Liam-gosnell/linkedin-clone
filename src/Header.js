246a4313ecb606ab8710dff7343756b46aa245ce
Liam-gosnell <liamgoz99@gmail.com>
2021-09-23 22:11:08 +0100

    Added Header option for right hand side of header, changed jshint settings, HeaderOption.css file added, import material-ui icons

--- src/Header.js
+++ src/Header.js
@@ -1,24 +1,31 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
+import HeaderOption from './HeaderOption';
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
 
 function Header() {
     return (
         <div className='header'>
-            <h1>This is the header</h1>
+
 
             <div className="header__left">
-                <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="" />
+
+                <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt=""/>
 
                 <div className="header__search">
-                <SearchIcon />
-                <input type="text" />
-            </div>
+                    <SearchIcon />
+                    <input type="text" />
+                </div>
+
             </div>
 
             <div className="header__right">
-
+                <HeaderOption Icon={HomeIcon} title='Home'/>
+                <HeaderOption Icon={SupervisorAccountIcon} title='My Network'/>
             </div>
+
         </div>
     );
 }
