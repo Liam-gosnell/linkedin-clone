ab9815b256a78a6e94e4436b43da2114a121ddae
Liam-gosnell <liamgoz99@gmail.com>
2021-09-27 19:29:57 +0000

    Added Jobs, Notifications & me sections to header, styles to header icons

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,9 @@ import SearchIcon from '@mui/icons-material/Search';
 import HeaderOption from './HeaderOption';
 import HomeIcon from "@material-ui/icons/Home";
 import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 function Header() {
     return (
@@ -24,6 +27,10 @@ function Header() {
             <div className="header__right">
                 <HeaderOption Icon={HomeIcon} title='Home'/>
                 <HeaderOption Icon={SupervisorAccountIcon} title='My Network'/>
+                <HeaderOption Icon={BusinessCenterIcon} title='Jobs'/>
+                <HeaderOption Icon={ChatIcon} title='Messaging'/>
+                <HeaderOption Icon={NotificationsIcon} title='Notifications'/>
+                <HeaderOption avatar="https://assets.website-files.com/5f2bd7b4c1df8f22e05f8c5f/5f33be338fa5cb2e2e792483_P1030380.jpg" title="me"/>
             </div>
 
         </div>
