2f6d410f4816a0b09de20852cf5097d31799e659
Liam-gosnell <liamgoz99@gmail.com>
2021-09-29 13:59:07 +0000

    added sidebar__stat under profile sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -7,10 +7,22 @@ function Sidebar() {
         <div className="sideBar">
            <div className=".sidebar__top" >
                <img src="" alt=""/>
-               <Avatar />
+               <Avatar className="sidebar__avatar"/>
                <h2>Liam Gosnell</h2>
                <h4>developer.liamg@gmail.com</h4>
            </div>
+
+           <div className=".sidebar__stats">
+                <div className=".sidebar__stat">
+                    <p>Who viewed you</p>
+                    <p className="sidebar__statNumber">2,562</p>
+                </div>
+                <div className=".sidebar__stat">
+                    <p>Views on post</p>
+                    <p className="sidebar__statNumber">2,446</p>
+                </div>
+           </div>
+
         </div>
     )
 }
