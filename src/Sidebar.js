23205d6c60f1eea8fe070f01c1d08c92ba5784f5
Liam-gosnell <liamgoz99@gmail.com>
2021-10-02 15:35:55 +0000

    added styles to app__body, sidebar__top

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,20 +5,20 @@ import './Sidebar.css';
 function Sidebar() {
     return (
         <div className="sideBar">
-           <div className=".sidebar__top" >
-               <img src="" alt=""/>
+           <div className="sidebar__top" >
+               <img src="https://images.unsplash.com/photo-1554034483-04fda0d3507b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80" alt=""/>
                <Avatar className="sidebar__avatar"/>
                <h2>Liam Gosnell</h2>
                <h4>developer.liamg@gmail.com</h4>
            </div>
 
-           <div className=".sidebar__stats">
-                <div className=".sidebar__stat">
+           <div className="sidebar__stats">
+                <div className="sidebar__stat">
                     <p>Who viewed you</p>
                     <p className="sidebar__statNumber">2,562</p>
                     
                 </div>
-                <div className=".sidebar__stat">
+                <div className="sidebar__stat">
                     <p>Views on post</p>
                     <p className="sidebar__statNumber">2,446</p>
                 </div>
