6c89cb33893189e119bfd1031b72e8649dc07682
Liam-gosnell <liamgoz99@gmail.com>
2021-10-11 16:16:22 +0100

    Added login.js / login.css pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import './App.css';
+import { useSelector } from 'react-redux';
 import Feed from "./Feed";
+import { selectUser } from "./features/userSlice";
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Login from "./Login.js";
 
 function App() {
+  const user = useSelector(selectUser);
+
+
   return (
     <div className="app">
       {/* Header */}
       <Header />
 
-      {/* App Body */}
-      <div className="app__body">
+      {!user ? (
+      <Login/> 
+      ) : (
+        <div className="app__body">
         <Sidebar />
         <Feed />
           {/* Widgets */}
       </div>
+      )}
     </div>
   );
 }
