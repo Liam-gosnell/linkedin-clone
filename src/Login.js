62cccdde0d4bc5806ca34714cc3d510d3132f15e
Liam-gosnell <liamgoz99@gmail.com>
2021-10-12 17:29:34 +0000

    Styles added to Login Form, added placeholder to header search icon

--- src/Login.js
+++ src/Login.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './Login.css';
 
 function Login() {
+    const loginToApp = () => {};
+    const register = () => {};
+
     return (
         <div className="login">
             <img
-                src="https://brandlogos.net/wp-content/uploads/2017/01/linkedin-logo-512x512.png"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/1200px-LinkedIn_Logo.svg.png"
                 alt=""
             />
 
@@ -14,11 +17,15 @@ function Login() {
 
                 <input placeholder="Profile pic URL (optional)" type="text"/>
 
-                <input placeholder="Email" type="text"/>
+                <input placeholder="Email" type="email"/>
 
                 <input placeholder="Password" type="password"/>
 
+                <button type="submit" onClick={loginToApp}> Sign In </button>
+
             </form>
+
+            <p>Not a member? <span className="login__register" onClick={register} >Register Now</span></p>
 
         </div>
     )
