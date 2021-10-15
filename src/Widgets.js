1242240ca19a3a2410e8861eb63d4e6643a35e3b
Liam-gosnell <liamgoz99@gmail.com>
2021-10-15 13:34:53 +0000

    Added styles to widgets, changed app__body width

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import "./Widgets.css";
 import InfoIcon from "@material-ui/icons/Info";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord"
 
 function Widgets() {
-    return <div className="widgets">
+
+    const newsArticle = (heading, subtitle) => (
+        <div className="widgets__article">
+            <div className="widgets__articleLeft">
+                <FiberManualRecordIcon />
+            </div>
+
+            <div className="widgets__articleRight">
+                <h4>{heading}</h4>
+                <p>{subtitle}</p>
+            </div>
+        </div>
+
+    );
+
+    return (
+    
+        <div className="widgets">
 
             <div className="widgets__header">
                 <h2>LinkedIn News</h2>
                 <InfoIcon />
             </div>
-        
-        </div>;
+        {newsArticle("Goz is back", "Top news - 9099 readers")}
+        {newsArticle("Coronavirus: IRE updates", "Top news - 886 readers")}
+        {newsArticle("Tesla hits new highs", "Car & auto news - 300 readers")}
+        {newsArticle("Sally Rooney Sells out again!", "Book news - 2080readers")}
+        {newsArticle("Bitcoin breaks $35k", "Crypto news - 8000 readers")}
+        {newsArticle("Record Sales of VW", "Car & auto news - 886 readers")}
+        </div>
+        );
 }
 
 export default Widgets;
